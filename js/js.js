7de88422481626dc5675b6ce3e7368d2a3af74b6
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-05-05 20:44:25 -0300

    adds interactive background svg

--- js/js.js
+++ js/js.js
@@ -1,7 +1,23 @@
 ---
-layout: none  
+layout: none
 ---
 
+let mButton;
+function makeButtonClickAgain() {
+  console.log('from MBCA');
+
+  const allButtons = document.getElementsByClassName('clickButton');
+  for(let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener('mousedown', function() {
+      console.log(this.getAttribute('data-text'));
+    }, false);
+
+    allButtons[i].addEventListener('mouseover', function() {
+      document.getElementById('my-center-text').innerHTML = this.getAttribute('data-text');
+    }, false);
+  }
+}
+
 window.onload = function() {
-  
+  makeButtonClickAgain();
 }
