6d39fb437b2ab3382b9d519bdc3589eb53d1e6cd
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-05-05 22:48:37 -0300

    clear center text on mouseout

--- js/js.js
+++ js/js.js
@@ -8,6 +8,11 @@ function buttonMouseover() {
   document.getElementById('my-center-text').innerHTML = this.getAttribute('data-text');
 }
 
+function buttonMouseout() {
+  document.getElementById('my-center-text').innerHTML = '';
+}
+
+
 function makeButtonClickAgain() {
   allButtons = document.getElementsByClassName('clickButton');
 
@@ -15,11 +20,13 @@ function makeButtonClickAgain() {
     allButtons[i].addEventListener('mousedown', function() {
       for(let j = 0; j < allButtons.length; j++) {
         allButtons[j].removeEventListener('mouseover', buttonMouseover);
+        allButtons[j].removeEventListener('mouseout', buttonMouseout);
       }
       document.getElementById('my-calendar-svg').classList.add('calendar-svg-hide');
     }, false);
 
     allButtons[i].addEventListener('mouseover', buttonMouseover, false);
+    allButtons[i].addEventListener('mouseout', buttonMouseout, false);
   }
 }
 
